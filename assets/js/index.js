544d2ba96f663f2a4d95bcdc94e6cef3cf026c62
Dipadana Putu <putudipadana@gmail.com>
2021-09-20 18:30:37 +0800

    get query

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,19 @@
-function fetchData() {
+const selectedProvince = document.querySelector("#selectProvinsi");
+const selectedCity = document.querySelector("#selectCity");
+const submitButton = document.querySelector("#submitButton");
+let provinceData = [];
+let p = '';
+let c = '';
+
+function fetchProvinceData() {
   fetch("https://api.vaksinasi.id/regions")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data.data);
-      const html = data.data
+      provinceData = data.data;
+      console.log(provinceData);
+      const province = data.data
         .map((data) => {
           return `
           <option> ${data.province} </option>
@@ -14,11 +22,44 @@ function fetchData() {
         .join("");
       document
         .querySelector("#selectProvinsi")
-        .insertAdjacentHTML("afterbegin", html);
+        .insertAdjacentHTML("afterbegin", province);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-fetchData();
+function getCity (province) {
+  let tempCity;
+  for (i = 0; i < provinceData.length; i++) {
+    if (provinceData[i].province === province) {
+      tempCity = provinceData[i].city
+        .map((city) => {
+          return `
+        <option> ${city} </option>
+        `;
+        })
+        .join("");
+    }
+  };
+  console.log(tempCity);
+  document
+    .querySelector("#selectCity")
+    .insertAdjacentHTML("afterbegin", tempCity);
+}
+
+selectedProvince.addEventListener("change", (event) => {
+  p = event.target.value;
+  getCity(event.target.value);
+})
+
+selectedCity.addEventListener("change", (event) => {
+  c = event.target.value;
+})
+
+submitButton.addEventListener("click", (event) => {
+  window.location.href = `../list.html?city=${c}&province=${p}`;
+})
+
+
+fetchProvinceData();
